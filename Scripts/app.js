2389679430a6da4b5fa0b95fec057264d51fec74
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-19 09:08:24 -0500

    added jquery, bootstrap and font-awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,17 @@
 /* custom JavaScript goes here */
+
+// IIFE - Immediately Invoked Function Expression
+// AKA: Anonymous Self-Executing Function
+// Also a "Closure"
+
+(function() 
+{
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+
+    window.addEventListener("load", Start);
+})();
