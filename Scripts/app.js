60924fbbc6635d272103e85a0a6b83f1d55153fa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-19 09:41:57 -0500

    added simple paragraph content to paragraphOne

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,6 +10,11 @@
     function Start()
     {
         console.log("App Started...");
+
+        let paragraphOneContent = "This is my first paragraph";
+
+        let paragraphOne = document.getElementById("paragraphOne");
+        paragraphOne.textContent = paragraphOneContent;
     }
 
 
